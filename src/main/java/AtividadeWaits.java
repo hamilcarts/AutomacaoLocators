adfe848cdcc3f3b37b25c65693c6c5e558b4400d
Hamilcar Tortora da Silva <109037178+hamilcarts@users.noreply.github.com>
2023-04-22 11:38:48 -0300

    Atividade Wait Explicito

--- src/main/java/AtividadeWaits.java
+++ src/main/java/AtividadeWaits.java
@@ -1,13 +1,11 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.*;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -16,7 +14,6 @@ public class AtividadeWaits {
     static WebDriver driver;
     private static final String URL = "https://chercher.tech/practice/explicit-wait-sample-selenium-webdriver";
     private WebDriverWait wait;
-    private Wait<WebDriver> waitFluente;
 
     @BeforeAll
     public static void beforeAll() {
@@ -25,10 +22,11 @@ public class AtividadeWaits {
         driver.manage().window().maximize();
     }
 
-//    @BeforeEach
-//    void beforeEach(){
-//    utilizado através dos métodos, para testar ambos na mesma classe
-//    }
+    @BeforeEach
+    public void beforeEach() {
+        driver.get(URL);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+    }
 
 
     @AfterAll
@@ -38,10 +36,10 @@ public class AtividadeWaits {
 
     //Sem Waits
     @Test
-    public void alertButtonTest() throws InterruptedException {
+    public void alertButtonTest() {
         WebElement element = driver.findElement(By.id("alert"));
         element.click();
-        Thread.sleep(5500);
+        wait.until(ExpectedConditions.alertIsPresent());
         Alert alert = driver.switchTo().alert();
         Assertions.assertEquals("I got opened after 5 secods", alert.getText());
         alert.accept();
@@ -50,7 +48,6 @@ public class AtividadeWaits {
     //Wait Explicito
     @Test
     public void titleButtonTest() {
-        beforWaitExplicito();
         WebElement titulo = driver.findElement(By.className("target-text"));
         Assertions.assertEquals("site", titulo.getText());
         driver.findElement(By.id("populate-text")).click();
@@ -60,7 +57,6 @@ public class AtividadeWaits {
 
     @Test
     public void displayButtonTest() {
-        beforWaitExplicito();
         driver.findElement(By.xpath("//*[@id=\"display-other-button\"]")).click();
         WebElement element = driver.findElement(By.xpath("//*[@id=\"hidden\"]"));
         wait.until(ExpectedConditions.visibilityOf(element));
@@ -69,34 +65,19 @@ public class AtividadeWaits {
     //Wait Fluente
     @Test
     public void enableButtonTest() {
-        beforWaitFluente();
         WebElement element = driver.findElement(By.id("enable-button"));
         element.click();
-        waitFluente.until(ExpectedConditions.elementToBeClickable(By.id("disable")));
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("disable")));
         WebElement disable = driver.findElement(By.id("disable"));
         Assertions.assertTrue(disable.isEnabled());
     }
 
     @Test
     public void checkBoxButtonTest() {
-        beforWaitFluente();
         WebElement element = driver.findElement(By.id("checkbox"));
         element.click();
         WebElement checkbox = driver.findElement(By.id("ch"));
-        waitFluente.until(ExpectedConditions.elementToBeSelected(checkbox));
+        wait.until(ExpectedConditions.elementToBeSelected(checkbox));
         Assertions.assertTrue(checkbox.isSelected());
-    }
-
-    private void beforWaitExplicito() {
-        driver.get(URL);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-    }
-
-    private void beforWaitFluente() {
-        waitFluente = new FluentWait<>(driver)
-                .withTimeout(Duration.ofSeconds(60))
-                .pollingEvery(Duration.ofMillis(100))
-                .ignoring(NoSuchElementException.class);
-        driver.get(URL);
     }
 }
